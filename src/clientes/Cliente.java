5fab3e4a6392ebe9fcda417f83dd7070f19d3e1c
Bryan <bryanlaraarguello@gmail.com>
2026-08-22 18:45:34 -0600

    Cambios en Clase Cliente

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -4,10 +4,58 @@
  */
 package clientes;
 
+import java.time.LocalDate;
+import java.time.Period;
+import personas.Persona;
+
 /**
  *
  * @author bryan
  */
-public class Cliente {
+public class Cliente extends Persona{
+    private LocalDate fechaNacimiento;
+    private String correoElectronico;
+ 
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(LocalDate fechaNacimiento, String correoElectronico, int ID, String nombre, String apellido) {
+        super(ID, nombre, apellido);
+        this.fechaNacimiento = fechaNacimiento;
+        this.correoElectronico = correoElectronico;
+    }
     
+    
+ 
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+ 
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+ 
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+ 
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+ 
+    public int calcularEdad() {
+        if (fechaNacimiento == null) {
+            return 0;
+        }
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "fechaNacimiento=" + fechaNacimiento + ", correoElectronico=" + correoElectronico + '}';
+    }
+    
+    
+   
 }
