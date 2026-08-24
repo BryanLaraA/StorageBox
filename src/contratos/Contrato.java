527558110ceeb8c09b1c985c374f415101ec4c46
FlyStudent <anporrasnu@est.utn.ac.cr>
2026-08-23 19:48:50 -0600

    Contrado
    se crea la clase de contrado con sus atributos y metodo constructor

--- src/contratos/Contrato.java
+++ src/contratos/Contrato.java
@@ -4,10 +4,34 @@
  */
 package contratos;
 
+import clientes.Cliente;
+import espacios.Espacio;
+import java.awt.List;
+import java.time.LocalDate;
+import servicios.Servicio;
+
 /**
  *
  * @author bryan
  */
 public class Contrato {
     private int numeroContrato;
+    private Cliente cliente;
+    private Espacio espacio;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private EstadoContrato estado;
+    private List serviciosAdicionales;
+    private double subTotal;
+    private double impuestos;
+    private double total;
+
+    public Contrato(Cliente cliente, Espacio espacio, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.cliente = cliente;
+        this.espacio = espacio;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+    
+    
 }
