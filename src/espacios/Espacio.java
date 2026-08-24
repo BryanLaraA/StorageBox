9650ebbcf9b59a61782335fe93fbdd1b46aa85a9
isaac <isaacpr11371@gmail.com>
2026-08-23 18:32:56 -0600

    Se eliminan metodos de la clase AdministradorTareas
    Se eliminan los metodos de la clase administrador de tareas porque los metodos pertenecian a la clase Espacio que seria el modelo

--- src/espacios/Espacio.java
+++ src/espacios/Espacio.java
@@ -1,11 +1,18 @@
 package espacios;
+
+import espacios.TipoEspacio;
+import excepciones.EspacioAlquiladoException;
+import excepciones.EspacioDuplicadoException;
+import java.util.LinkedList;
+
 public class Espacio {
-    
+    private LinkedList<Espacio> espacios;
     private int numero; 
     private TipoEspacio tipo; 
     private double capacidad; 
     private double precioMensual; 
     private boolean disponible; 
+    
 
     public int getNumero() {
         return numero;
@@ -39,13 +46,16 @@ public class Espacio {
         this.precioMensual = precioMensual;
     }
 
-    public Espacio(int numero, TipoEspacio tipo, double capacidad, double precioMensual) {
+    public Espacio(LinkedList<Espacio> espacios, int numero, TipoEspacio tipo, double capacidad, double precioMensual) {
+        this.espacios = new LinkedList<>();
         this.numero = numero;
         this.tipo = tipo;
         this.capacidad = capacidad;
         this.precioMensual = precioMensual;
         this.disponible = true;
     }
+
+   
 
     public void asignarEspacio(){
         switch (tipo){
@@ -66,18 +76,114 @@ public class Espacio {
         }
     }   
     
-    public void cambiarTipo(TipoEspacio nuevoTipo){
-        this.tipo =nuevoTipo; 
+  public void cambiarTipo(TipoEspacio nuevoTipo){
+       this.tipo =nuevoTipo; 
         asignarEspacio();
     }
     
-    public void ocupar(){
+  public void ocupar(){
         this.disponible = false; 
     }
     
-    public void cambiardisponible(){
+  public void cambiardisponible(){
         this.disponible = true; 
     }
+    
+  public void agregarEspacio(Espacio nuevoEspacio) throws EspacioDuplicadoException {
+    for (Espacio espacio : espacios){
+        
+        if (espacio.getNumero() == nuevoEspacio.getNumero()) {
+         throw new EspacioDuplicadoException();
+        }
+    }
+    espacios.add(nuevoEspacio);
+}  
+ 
+    public Espacio buscarNumero(int numero){
+    for (Espacio espacio : espacios){
+        if (espacio.getNumero() == numero){
+            return espacio; 
+        }
+    }
+    return null; 
+}
+
+
+    public boolean actualizarEspacio(int numero,TipoEspacio tipo, double capacidad, double precio){
+    Espacio espacio = buscarNumero(numero); 
+    if (espacio == null) {
+        return false;  
+    }
+    espacio.setTipo(tipo);
+    espacio.setCapacidad(capacidad);
+    espacio.setPrecioMensual(precio);
+    
+    return true; 
+}
+
+    public void eliminarEspacio(int numero)throws EspacioAlquiladoException{
+    Espacio espacio = buscarNumero(numero);
+    
+        if (espacio != null){
+        if (!espacio.isDisponible()) {
+            throw new EspacioAlquiladoException(); 
+            
+            }
+        }
+        espacios.remove(espacio);
+}
+
+    public LinkedList<Espacio> filtrarTipo(TipoEspacio tipo) {
+    LinkedList<Espacio> resultados = new LinkedList<>();
+
+    for (Espacio espacio : espacios) {
+
+        if (espacio.getTipo() == tipo) {
+            resultados.add(espacio);
+        }
+
+    }
+
+    return resultados;
+} 
+
+    public LinkedList<Espacio> filtrarDisponibilidad(boolean disponible) {
+
+    LinkedList<Espacio> resultados = new LinkedList<>();
+
+    for (Espacio espacio : espacios) {
+
+        if (espacio.isDisponible() == disponible) {
+            resultados.add(espacio);
+        }
+
+    }
+
+    return resultados;
+}
+
+    public LinkedList<Espacio> filtrarRangoPrecio(double minimo, double maximo) {
+
+    LinkedList<Espacio> resultados = new LinkedList<>();
+
+    for (Espacio espacio : espacios) {
+
+        if (espacio.getPrecioMensual() >= minimo &&
+            espacio.getPrecioMensual() <= maximo) {
+
+            resultados.add(espacio);
+        }
+
+    }
+
+    return resultados;
+}
+
+    public LinkedList<Espacio> obtEspacios() {
+    return espacios;
+}
+
+
 
     @Override
     public String toString() {
@@ -89,5 +195,4 @@ public class Espacio {
                 disponible + '}';
     }
     
-  
 }
