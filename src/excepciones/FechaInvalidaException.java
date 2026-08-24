36e13a8fd769613f127a4435342f8428f68d743e
FlyStudent <anporrasnu@est.utn.ac.cr>
2026-08-23 21:00:00 -0600

    Excepcionnes Estado invalido y fecha invalida

---
+++ src/excepciones/FechaInvalidaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author andre
+ */
+public class FechaInvalidaException extends Exception {
+
+    public FechaInvalidaException(String mensaje) {
+        super(mensaje);
+    }
+}
